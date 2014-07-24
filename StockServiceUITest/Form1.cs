9154f0e0b059e19763ac0cd91656137d2d480f65
shenghai <766058573@qq.com>
2014-07-24 16:48:17 +0800

    add HttpWebResponseUtility.cs (Get Post)

--- StockServiceUITest/Form1.cs
+++ StockServiceUITest/Form1.cs
@@ -7,6 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.ServiceProcess;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+//using System.DirectoryServices.Protocols;
+//using System.ServiceModel.Security;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;  
 
 namespace StockServiceUITest
 {
@@ -19,8 +27,39 @@ namespace StockServiceUITest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ServiceController controller = new ServiceController("StockService");
-            controller.Stop();
+            //-----------------------------------------------------------------------------
+            //ServiceController controller = new ServiceController("StockService");
+            //controller.Stop();
+
+            //-----------------------------------------------------------------------------
+
+           /* string loginUrl = "https://passport.baidu.com/?login";
+            string userName = "15528358573";
+            string password = "123465";
+            string tagUrl = "http://cang.baidu.com/" + userName + "/tags";
+            Encoding encoding = Encoding.GetEncoding("gb2312");
+
+            IDictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("tpl", "fa");
+            parameters.Add("tpl_reg", "fa");
+            parameters.Add("u", tagUrl);
+            parameters.Add("psp_tt", "0");
+            parameters.Add("username", userName);
+            parameters.Add("password", password);
+            parameters.Add("mem_pass", "1");
+            HttpWebResponse response = HttpWebResponseUtility.CreatePostHttpResponse(loginUrl, parameters, null, null, encoding, null);
+            string cookieString = response.Headers["Set-Cookie"];*/
+            //---------------------------------------------------------------------------------
+
+            string userName = "15528358573";
+            string tagUrl = "http://www.sina.com/";// +userName + "/tags";
+            CookieCollection cookies = new CookieCollection();//如何从response.Headers["Set-Cookie"];中获取并设置CookieCollection的代码略  
+            HttpWebResponse response = HttpWebResponseUtility.CreateGetHttpResponse(tagUrl, null, null, cookies);
+            Stream stream = response.GetResponseStream();
+           
+            stream.ReadTimeout = 15 * 1000; //读取超时
+            StreamReader sr = new StreamReader(stream, Encoding.GetEncoding("gb2312"));
+            string strWebData = sr.ReadToEnd();
         }
     }
 }
